4448c567d0c531a490b9dee04f7abb3ff8ac79fd
Opentech4uco <opentech4u@gmail.comgit config --global user.name Opentech4ucogit config --global user.email opentech4u@gmail.com>
2021-03-04 18:40:33 +0530

    fron page Tan 04.03.2021

--- application/views/department.php
+++ application/views/department.php
@@ -1,102 +1,78 @@
+<!doctype html>
 <html>
-<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link rel="icon" href="favicon.ico">
- 
- <!--<meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
- <link rel="stylesheet" type="text/css" href="<?php echo base_url("/assets/css/style1.css");?>">
- <title>The West Bengal State Co-operative Marketing Federation Ltd.</title>
+<head>
+<meta charset="utf-8">
+<title>Benfed</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="shortcut icon" href="<?php echo base_url("/assets/front_page/images/favicon.png");?>" type="image/x-icon"> 
+<link rel="stylesheet" type="text/css" href="<?php echo base_url("/assets/front_page/css/bootstrap.css"); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url("/assets/front_page/css/font-awesome.css"); ?>">
+	
+<link rel="stylesheet" type="text/css" href="<?php echo base_url("/assets/front_page/css/apps.css"); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url("/assets/front_page/css/apps_inner.css"); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url("/assets/front_page/css/res.css"); ?>">
+
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+<!--    font-family: 'Roboto', sans-serif;-->
+
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+<!--font-family: 'Oswald', sans-serif;-->
+	
 </head>
-<style>
-div.block {
-	margin: 20px;
-   height: 195px;
-   border: 10px;
-   text-align: center;
-   padding: 55px;
-	flex: 1;
-	border-radius: 25px;
-	color: white;
-}
 
-div.block:active{
-	transform: translateY(4px);
-}
-
-a{
-	font-size: 25px;
-}
-a:hover{
-	text-decoration: none;
-}
-.block:hover{
-
-	-webkit-box-shadow: 5px 5px 30px 20px #CCCCCC;
-	box-shadow: 5px 5px 30px 20px #CCCCCC;
-
-}
-</style>
-<body style="background-image: url('<?=base_url()?>paddy.jpg'); background-size: 100% 100%;">
-	<div >
-    	<div class="header">
-    		<div class="row">
-        <div class="col-md-4"></div><div class="col-md-1">  </div>
-        <div class="col-md-4"><img src="<?=base_url()?>benfed.png"/> </div>
-    	    </div>
-    	</div>
-		 
-		<div class="row">
-		
-		 <div class="col-md-4">
-			<a class="text-white hover" href="<?=base_url()?>index.php/User_Login/login">
-				<div class="block index_btn1">
-					Paddy<br>Procurement
-				</div>    
-			</a>
-		 </div>  
-		 <!--<div class="col-md-4">
-			<a class="text-white" href="<?=base_url()?>index.php/Finance_Login/login">
-				<div class="block index_btn2">
-					Finance<br>Management
-				</div>  
-			</a>	
-		 </div>-->
-	    <div class="col-md-4">
-			<a class="text-white" href="<?=base_url()?>index.php/Fertilizer_Login/login">
-				<div class="block index_btn3">
-					Fertilizer<br>Management
-				</div>
-			</a>  
-		 </div>
-		</div>
-
-		<!-- 		<div class="row">
-				 <div class="col-md-4">
-					<a class="text-white" href="#">
-						<div class="block index_btn4">
-						Demo
-						</div>
-					</a> 
-				 </div>  
-				 <div class="col-md-4">
-					<a class="text-white" href="#">
-						<div class="block index_btn5">
-						Softek
-						</div>
-					</a>
-				 </div>
-			   
-				 <div class="col-md-4">
-					<a class="text-white" href="http://www.synergicportal.in/mkt">
-						<div class="block index_btn7">
-						 Marketing
-						</div>
-					</a>  
-				 </div>
-				</div>  -->    
+<body>
+<header class="headerTop">
+	<div class="wrapper">
+	<div class="col-sm-3 float-left logo"><img src="<?php echo base_url("/assets/front_page/images/logo.png"); ?>" alt=""/></div>
+	<div class="col-sm-9 float-left rightTxt">
+	<h2>The W.B.S Co-Operative Marketing Federation Ltd (Benfed)<br>
+	<span>Welcome To Benfed ePortal</span></h2>	
 	</div>
+	</div>	
+</header>
+	
+<div class="daseboardContentArea daseboardPading">
+	<div class="wrapper">
+		<div class="col-sm-6 float-left">
+
+			<a href="<?=base_url()?>index.php/User_Login/login"> 
+		
+				<div class="boxSec1"><h2>Paddy Procurement <span>Manage Paddy Procurement Operations</span></h2></div>
+			</a>
+
+		</div>
+		
+		<div class="col-sm-6 float-left">
+
+			<a href="<?=base_url()?>fertilizer/index.php/Fertilizer_Login/login">
+
+				<div class="boxSec2"><h2>Fertilizer Management <span>Manage Fertilizer Operations</span></h2></div>
+			</a>
+
+		</div>
+		
+	</div>
+</div>
+	
+<footer class="footerSec">
+	<div class="wrapper">
+	<div class="col-sm-6 float-left mapSec"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3685.7375062895694!2d88.39095591548983!3d22.5140296852129!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a02714d6e5d2557%3A0x384d7dbe8ab31a73!2s3rd%20Floor%2C%201582%2C%20Rajdanga%20Main%20Rd%2C%20Kasba%20New%20Market%2C%20Rajdanga%2C%20Kasba%2C%20Kolkata%2C%20West%20Bengal%20700107!5e0!3m2!1sen!2sin!4v1614851691132!5m2!1sen!2sin" width="100%" height="195" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div>
+	<div class="col-sm-6 float-left addressSec">
+		<h2>Location</h2>
+		<p>Southend Conclave, 3rd Floor, 1582 Rajdanga Main Road, <br>
+			Kolkata - 700 107</p>
+		<ul>
+		<li><i class="fa fa-phone" aria-hidden="true"></i> 91 33 2441 4366/67</li>
+		<li><i class="fa fa-fax" aria-hidden="true"></i> +91 33 2441-4372</li>
+		<li><i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:info@benfed.org">info@benfed.org</a></li>
+		</ul>
+		
+	</div>
+	</div>
+</footer>
 </body>
 </html>
